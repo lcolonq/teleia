b8ec915593c81b7a5d0d497100ee4b9b1510cb55
LLLL Colonq <llll@colonq>
2025-12-26 18:52:29 -0500

    Make Error msg field public

--- crates/teleia/src/utils.rs
+++ crates/teleia/src/utils.rs
@@ -12,7 +12,7 @@ pub fn erm<E, T>(e: E) -> Erm<T> where E: std::error::Error + std::marker::Send 
 
 #[derive(Debug)]
 pub struct Error {
-    msg: String
+    pub msg: String
 }
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
