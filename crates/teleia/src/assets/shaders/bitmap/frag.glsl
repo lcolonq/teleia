f06acba9390f3f579dd551c49dd61b8575ea342b
LLLL Colonq <llll@colonq>
2026-06-12 16:04:11 -0400

    Update

--- crates/teleia/src/assets/shaders/bitmap/frag.glsl
+++ crates/teleia/src/assets/shaders/bitmap/frag.glsl
@@ -2,6 +2,7 @@
 precision highp float;
 
 uniform sampler2D texture_data;
+uniform vec4 background;
 
 in vec2 vertex_texcoord;
 in vec3 vertex_color;
@@ -9,7 +10,13 @@ out vec4 frag_color;
 
 void main() {
     vec4 texel = texture(texture_data, vertex_texcoord);
-    if (texel.rgb == vec3(0.0, 0.0, 0.0)) discard;
+    if (vertex_color == vec3(0.0, 0.0, 0.0)) {
+        discard;
+    }
+    if (texel.rgb == vec3(0.0, 0.0, 0.0)) {
+        frag_color = background;
+        return;
+    }
     texel.rgb = vertex_color;
     frag_color = texel;
 } 
